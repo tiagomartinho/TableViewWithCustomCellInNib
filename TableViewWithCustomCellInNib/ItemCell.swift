25441a49d7b37f953aea5fd30b62c72a0c6a6995
tiagomartinho <tiago.martinho@xpeppers.com>
2015-11-13 10:18:52 +0100

    initial commit

---
+++ TableViewWithCustomCellInNib/ItemCell.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class ItemCell:UITableViewCell {
+    
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var cellDescription: UILabel!
+}
--- TableViewWithCustomCellInNib/ViewController.swift
+++ TableViewWithCustomCellInNib/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class TableViewController: UITableViewController {
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let nib = UINib(nibName: "ItemCell", bundle: nil)
+        self.tableView.registerNib(nib, forCellReuseIdentifier: "itemCell")
+        let cell = tableView.dequeueReusableCellWithIdentifier("itemCell") as! ItemCell
+        
+        var text = ""
+        for _ in 1...arc4random_uniform(100) {
+            text += "dskfj"
+        }
+        cell.cellDescription.text = text
+        if indexPath.row % 2 == 0 {
+            cell.cellImage.image = UIImage(named: "scan")
+        }
+        else {
+            cell.cellImage.image = UIImage(named: "scan2")
+        }
+        return cell
     }
-
-
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    override func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 400.0
+    }
 }
 
